c1d37ae3ef16638fe8e47c14592b8a615662350a
ifeanyi_ns <ifeanyi_ns@icloud.com>
2022-12-02 01:10:58 +0100

    finalisation

--- cart.js
+++
@@ -1,7 +0,0 @@
-let basket = JSON.parse(localStorage.getItem("data")) || []
-
-let calculation = () =>{
-    let cartIcon = document.getElementById('cartAmount');
-    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, )
-}
-calculation()
--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
 let shop = document.getElementById('shop');
   
-let shopItemsData = [
-{
-    id:'fdse',
-    name:'Bow Tie',
-    price: 45,
-    desc: 'Lorem ipsum, dolor sit amet consectetur obcaecati architecto beatae quis, enim quas mollitia! Eius, excepturi!',
-    img: 'images/download.JPEG'
-},
-{
-    id:'fdsje',
-    name:'Hugo Boss Suit',
-    price: 1000,
-    desc: 'Lorem ipsum, dolor sit amet consectetur obcaecati architecto beatae quis, enim quas mollitia! Eius, excepturi!',
-    img: 'images/download2.JPEG'
-},
-{
-    id:'fdbse',
-    name:'Men Shoes',
-    price: 100,
-    desc: 'Lorem ipsum, dolor sit amet consectetur obcaecati architecto beatae quis, enim quas mollitia! Eius, excepturi!',
-    img: 'images/download3.JPEG'
-},
-{
-    id:'fdsbe',
-    name:' NS Air',
-    price: 120,
-    desc: 'Lorem ipsum, dolor sit amet consectetur obcaecati architecto beatae quis, enim quas mollitia! Eius, excepturi!',
-    img: 'images/download4.JPEG'
-}
-];
 
 let basket = JSON.parse(localStorage.getItem("data")) || []
 
+let generateShop =()=>{
+return (shop.innerHTML = shopItemsData.map ((x)=>{
+    let {id,price,name,desc,img } = x;
+    let search = basket.find((x)=>x.id === id) || [];
+    return `
+    <div id= product-id-${id} class="item"><img src=${img}alt="" class="imgs">
+        <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+                <h2>$ ${price}</h2>
+                <div class="buttons">
+                    <i onclick="decrement(${id})" class="bi bi-dash-circle"></i>
+                    <div id=${id} class="quantity">${search.item === undefined ? 0 : search.item}</div>
+                    <i onclick="increment(${id})"class="bi bi-plus-circle"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+}).join(''));
+};
 
 generateShop();
 
@@ -76,6 +68,6 @@ let update =(id)=>{
 
 let calculation = () =>{
     let cartIcon = document.getElementById('cartAmount');
-    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, )
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0 )
 }
-calculation()
+calculation();
